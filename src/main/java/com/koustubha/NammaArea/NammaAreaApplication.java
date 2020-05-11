7963d65a6d2a8f011313be07b31cf0c6c1b3819e
R Koustubha <koustubhar@gmail.com>
2020-05-11 14:34:43 +0530

    Pushing a empty Namma area project

---
+++ src/main/java/com/koustubha/NammaArea/NammaAreaApplication.java
@@ -0,0 +1,13 @@
+package com.koustubha.NammaArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NammaAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NammaAreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koustubha/NammaArea/NammaAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koustubha.NammaArea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NammaAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
